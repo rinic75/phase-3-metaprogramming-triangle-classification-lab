e63f0c11c8729928db9e5677577773e811fed83f
Chang Shin <cshin7374@gmail.com>
2022-09-18 19:59:17 -0500

    commit tree 1df8abda1f0ffa2470f7454498dfd6e97fb033ba

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    valid_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangle
+    raise TriangleError unless [a, b, c].all?(&:positive?) && (a + b > c && a + c > b && b + c > a)
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
